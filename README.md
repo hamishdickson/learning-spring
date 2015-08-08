f8d1fda6a6992326d161bac1bb9d63f658670d9a
hamishdickson <hamish.dickson@gmail.com>
2015-08-08 10:21:39 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# learning-spring
+
+A simple project (with lots of notes) for learning more about spring
---
+++ src/main/java/LearningSpring/App.java
@@ -0,0 +1,13 @@
+package LearningSpring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
