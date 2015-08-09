135ec987ec87708bd657f5e17c75dd1979e755fb
hamishdickson <hamish.dickson@gmail.com>
2015-08-09 13:49:18 +0100

    convert Employee from pojo to entity

--- src/main/java/LearningSpring/com/github/hamishdickson/timesheet/domain/Employee.java
+++ src/main/java/LearningSpring/com/github/hamishdickson/timesheet/domain/Employee.java
@@ -1,19 +1,70 @@
 package LearningSpring.com.github.hamishdickson.timesheet.domain;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employee")
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String name;
     private String department;
+
+    // required for hibernate
+    public Employee() {
+    }
 
     public Employee(String name, String department) {
         this.name = name;
         this.department = department;
     }
 
+    // getters and setters required for hibernate
     public String getName() {
         return name;
     }
 
     public String getDepartment() {
         return department;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Employee employee = (Employee) o;
+
+        if (id != null ? !id.equals(employee.id) : employee.id != null) return false;
+        if (name != null ? !name.equals(employee.name) : employee.name != null) return false;
+        return !(department != null ? !department.equals(employee.department) : employee.department != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (department != null ? department.hashCode() : 0);
+        return result;
     }
 }
---
+++ src/main/java/LearningSpring/com/github/hamishdickson/timesheet/domain/README.md
@@ -0,0 +1,12 @@
+# Entities
+
+Start out with POJOs -> Entities
+
+To do this:
+
+- Add default no-arg consturctors for hibernate
+- create getters and setters for fields
+- add equals and hashcode
+- add persistence annotations
+- add @Table to distinguish between java and sql naming conventions
+- add ids - primary keys for our relational database
