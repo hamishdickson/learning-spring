0aa528ca0c04efebb2f647e6546e7d3669b8e9d4
hamishdickson <hamish.dickson@gmail.com>
2015-08-09 14:00:02 +0100

    convert timesheet

--- src/main/java/LearningSpring/com/github/hamishdickson/timesheet/domain/Timesheet.java
+++ src/main/java/LearningSpring/com/github/hamishdickson/timesheet/domain/Timesheet.java
@@ -1,14 +1,37 @@
 package LearningSpring.com.github.hamishdickson.timesheet.domain;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "timesheet")
 public class Timesheet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "employee_id")
     private Employee who;
+
+    @OneToOne
+    @JoinColumn(name = "task_id")
     private Task task;
+
     private Integer hours;
+
+    public Timesheet() {
+    }
 
     public Timesheet(Employee who, Task task, Integer hours) {
         this.who = who;
         this.task = task;
         this.hours = hours;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public Employee getWho() {
@@ -21,6 +44,22 @@ public class Timesheet {
 
     public Integer getHours() {
         return hours;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setWho(Employee who) {
+        this.who = who;
+    }
+
+    public void setTask(Task task) {
+        this.task = task;
+    }
+
+    public void setHours(Integer hours) {
+        this.hours = hours;
     }
 
     /**
@@ -37,5 +76,28 @@ public class Timesheet {
                 ", task=" + task +
                 ", hours=" + hours +
                 ']';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Timesheet timesheet = (Timesheet) o;
+
+        if (id != null ? !id.equals(timesheet.id) : timesheet.id != null) return false;
+        if (who != null ? !who.equals(timesheet.who) : timesheet.who != null) return false;
+        if (task != null ? !task.equals(timesheet.task) : timesheet.task != null) return false;
+        return !(hours != null ? !hours.equals(timesheet.hours) : timesheet.hours != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (who != null ? who.hashCode() : 0);
+        result = 31 * result + (task != null ? task.hashCode() : 0);
+        result = 31 * result + (hours != null ? hours.hashCode() : 0);
+        return result;
     }
 }
