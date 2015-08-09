b7e27fc0722f28f56519dcf48239bba531109998
hamishdickson <hamish.dickson@gmail.com>
2015-08-09 15:07:29 +0100

    create hibernate dao

--- src/test/java/LearningSpring/com/github/hamishdickson/timesheet/service/impl/HibernateConfigurationTest.java
+++ src/test/java/LearningSpring/com/github/hamishdickson/timesheet/service/impl/HibernateConfigurationTest.java
@@ -1,14 +1,14 @@
 package LearningSpring.com.github.hamishdickson.timesheet.service.impl;
 
-import static org.junit.Assert.*;
-
 import org.hibernate.SessionFactory;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 
-@ContextConfiguration(locations = '/persistence-beans.xml')
+import static org.junit.Assert.assertNotNull;
+
+@ContextConfiguration(locations = "/persistence-beans.xml")
 public class HibernateConfigurationTest extends AbstractJUnit4SpringContextTests {
 
     @Autowired
